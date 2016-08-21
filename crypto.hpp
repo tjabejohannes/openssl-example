1e154ad4ee914fe11bf6d922d0b127efcd991354
Ole Christian Eidheim <eidheim@gmail.com>
2016-08-21 20:16:37 +0200

    Initial commit

---
+++ crypto.hpp
@@ -0,0 +1,57 @@
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
+#include <string>
+
+// Note: C-style casts, for instance (int), are used to simplify the source code.
+//       C++ casts, such as static_cast and reinterpret_cast, should otherwise
+//       be used in modern C++.
+
+/// Limited C++ bindings for the OpenSSL Crypto functions.
+class Crypto {
+public:
+  /// Return hex string from bytes in input string.
+  static std::string hex(const std::string &input) {
+    std::stringstream hex_stream;
+    hex_stream << std::hex << std::internal << std::setfill('0');
+    for (auto &byte : input)
+      hex_stream << std::setw(2) << (int)(unsigned char)byte;
+    return hex_stream.str();
+  }
+
+  /// Return the MD5 (128-bit) hash from input.
+  static std::string md5(const std::string &input) {
+    throw std::logic_error("not yet implemented");
+  }
+
+  /// Return the SHA-1 (160-bit) hash from input.
+  static std::string sha1(const std::string &input) {
+    std::string hash;
+    hash.resize(160 / 8);
+    SHA1((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    return hash;
+  }
+
+  /// Return the SHA-256 (256-bit) hash from input.
+  static std::string sha256(const std::string &input) {
+    throw std::logic_error("not yet implemented");
+  }
+
+  /// Return the SHA-512 (512-bit) hash from input.
+  static std::string sha512(const std::string &input) {
+    throw std::logic_error("not yet implemented");
+  }
+
+  /// Return key from the Password-Based Key Derivation Function 2 (PBKDF2).
+  static std::string pbkdf2(const std::string &password, const std::string &salt, int iterations = 4096, int key_length = 256 / 8) {
+    std::string key;
+    key.resize(key_length);
+    auto success = PKCS5_PBKDF2_HMAC_SHA1(password.data(), password.size(),
+                                          (const unsigned char *)salt.data(), salt.size(), iterations,
+                                          key_length, (unsigned char *)key.data());
+    if (!success)
+      throw std::runtime_error("openssl: error calling PBKCS5_PBKDF2_HMAC_SHA1");
+    return key;
+  }
+};
---
+++ examples.cpp
@@ -0,0 +1,15 @@
+#include "crypto.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "SHA-1 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha1("Test")) << endl << endl;
+  
+  cout << "SHA-1 with two iterations" << endl;
+  cout << Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) << endl;
+
+  cout << "The derived key from the PBKDF2 algorithm" << endl;
+  cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl;
+}
